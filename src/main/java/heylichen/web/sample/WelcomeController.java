d0fc79695fbdb61521c3f10607c143296c55a772
heylichen <wow123456>
2016-05-14 22:08:29 +0800

    A quickstart Spring Mvc 4 Sample. support restful interface.

---
+++ src/main/java/heylichen/web/sample/WelcomeController.java
@@ -0,0 +1,27 @@
+package heylichen.web.sample;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class WelcomeController {
+    // private final Logger logger = LoggerFactory.getLogger(WelcomeController.class);
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index() {
+        return "Index";
+    }
+
+    @RequestMapping(value = "/rest", method = RequestMethod.GET)
+    @ResponseBody
+    public Map restIndex() {
+        Map<String, String> map = new HashMap<>();
+        map.put("message", "hello, this web support restful service!");
+        return map;
+    }
+}
---
+++ src/main/webapp/WEB-INF/pages/Index.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Hello,This is a Spring Mvc Lab</title>
+</head>
+<body>
+<h1>Hello,This is a Spring Mvc Lab</h1>
+</body>
+</html>
